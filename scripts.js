1939f12a8e3dd922584ad00fe8f8daea6ee8bba1
mauro Leos <mauroleos94@gmail.com>
2020-02-10 19:55:55 -0600

    Added Javascript

---
+++ scripts.js
@@ -0,0 +1,9 @@
+setInterval(DigitalClock, 1000)
+
+function DigitalClock {
+  const currentDate = new Date();
+  const seconds = currentDate.getSeconds();
+  const minutes = currentDate.getMinutes();
+  const hours = currentDate.getHours();
+
+}
