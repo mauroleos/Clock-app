cdf6becff9a8b7f9e909d58c50a9daf51d8557ba
mauro Leos <mauroleos94@gmail.com>
2020-02-10 21:01:11 -0600

    testing javascript

--- scripts.js
+++ scripts.js
@@ -1,11 +1,1 @@
-function DigitalTime(){
-
-  var date = new Date()
-  var h = date.gethours();
-  var m = date.getminutes();
-  var s = date.getseconds();
-
-  var time = h + ":" + m + ":" + s;
-  document.getElementById("Clock-time").innerText = time;
-
-return DigitalTime();
+alert('File Linked');
